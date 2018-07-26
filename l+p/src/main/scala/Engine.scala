b7a855f64ee9d99b04c6640433dd3d6680f17fac
mike-seger <mss@unknown.com>
2018-07-27 00:49:40 +0200

    cleaned up

--- l+p/src/main/scala/Engine.scala
+++ l+p/src/main/scala/Engine.scala
@@ -2,8 +2,6 @@ import io.gatling.app.Gatling
 import io.gatling.core.config.GatlingPropertiesBuilder
 
 object Engine extends App {
-
-	System.out.println(System.getProperty("simulationClass"));
 	val props = new GatlingPropertiesBuilder()
 		.dataDirectory(System.getProperty("dataDirectory"))
 		.resultsDirectory(System.getProperty("resultsDirectory"))
